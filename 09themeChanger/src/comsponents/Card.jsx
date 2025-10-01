c9e292bb2899ac730339597c0dff73d88f251b08
Souma061 <soumabrataghosh57@gmail.com>
2025-10-01 22:47:18 +0530

    feat: update Card component with new image and title

--- 09themeChanger/src/comsponents/Card.jsx
+++ 09themeChanger/src/comsponents/Card.jsx
@@ -4,12 +4,12 @@ export default function Card() {
     return (
         <div className="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <a href="/">
-                <img className="p-8 rounded-t-lg" src="https://images.pexels.com/photos/18264716/pexels-photo-18264716/free-photo-of-man-people-laptop-internet.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="product_image1" />
+                <img className="p-8 rounded-t-lg" src="https://images.unsplash.com/photo-1626025437642-0b05076ca301?q=80&w=534&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="product_image1" />
             </a>
             <div className="px-5 pb-5">
                 <a href="/">
                     <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-                        Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
+                        The Crown Jewel for Argentina
                     </h5>
                 </a>
                 <div className="flex items-center mt-2.5 mb-5">
