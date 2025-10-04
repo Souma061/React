017a296d7f562615fc2ac7d41f61e965a7617339
Souma061 <soumabrataghosh57@gmail.com>
2025-10-04 20:38:02 +0530

    feat: implement AuthService and Service classes for user authentication and post management

---
+++ BlogApp/src/Appwrite/auth.js
@@ -0,0 +1,56 @@
+import { Account, Client, ID } from 'appwrite';
+import conf from '../config/config.js';
+
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client.setEndpoint(conf.appwriteUrl) // Your API Endpoint
+      .setProject(conf.appwriteProjectId); // Your project ID
+    this.account = new Account(this.client);
+  }
+
+  async createAccount({ email, password, name}) {
+    try {
+      const userAccount = await this.account.create(ID.unique(), email, password, name);
+      if (userAccount) {
+        // another method
+        return this.login({ email, password });
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailPasswordSession(email, password);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+       return await this.account.get();
+    } catch (error) {
+      console.log("Appwrite service :: getCurrentUser :: error", error);
+    }
+    return null;
+  }
+
+  async logOut() {
+    try {
+      await this.account.deleteSessions();
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+const authService = new AuthService();
+
+export default authService;
